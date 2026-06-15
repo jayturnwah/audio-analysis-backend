d6d2dd0cd6a7c16f6f8fc0d36e5a1134d16b3a36
Justin Ternois <jternois1000@gmail.com>
2026-06-14 19:06:17 -0600

    Use Render database URL

--- app/database.py
+++ app/database.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://justinternois@localhost:5432/evercurrent"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://justinternois@localhost:5432/evercurrent"
+)
 
 engine = create_engine(DATABASE_URL)
 
